66a82a8436cc243bf0f4f5ff70bacacdc43aac8e
agro40tech <volkovvova67@gmail.com>
2024-10-16 15:53:28 +0400

    feat: добавляет компонент about

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -3,12 +3,14 @@ import { FC } from 'react'
 import style from './style.module.scss'
 import { Header } from '@components/ui/header'
 import { Hero } from '@components/ui/hero'
+import { About } from '@components/ui/about'
 
 export const HomePage: FC = () => (
 	<div className={style.page}>
 		<Header />
-		<main>
+		<main className={style.main}>
 			<Hero />
+			<About />
 		</main>
 	</div>
 )
